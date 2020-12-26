39b6fae694e30ff6823a4241b7a3d0191c520b23
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2020-12-25 20:13:46 -0500

    Made a new components folder, changed the way the buttons look and moved the mango array into its own file

--- src/App.js
+++ src/App.js
@@ -1,32 +1,5 @@
 import './App.css';
-
-// const mangoArray = [
-//   {
-//       name: 'Sweet Tart',
-//       image: 'https://i.postimg.cc/zDcjj1Xj/sweet-tart.jpg',
-//       description: 'Sweet tart mango was developed by the Zill nursery in Boyton Beach, Florida. It\'s a seedling of Zill Indochinese and tastes like an amazing candy. Smooth and lives up to the name.'
-//   },
-//   {
-//       name: 'Glenn',
-//       image: 'https://i.postimg.cc/QCwJxcnP/glenn.jpg',
-//       description: 'Glenn is a delicious mango that was brought to Miami in the 1940\'s. Such a smooth mango, just a delight!'
-//   },
-//   {
-//       name: 'Cushman',
-//       image: 'https://i.postimg.cc/SNnZFxt3/cushman.jpg',
-//       description: 'Cushman is a big mango with an unusual round shape. Fiberless inside, the flavor is rich and sweet.'
-//   },
-//   {
-//       name: 'Pineapple Pleasure',
-//       image: 'https://i.postimg.cc/4xtByFG1/pineapple-pleasure.jpg',
-//       description: 'This mango has pineapple notes that will make you think twice about what fruit you\'re eating! A great balance of sweet and tart flavors make this mango a winner.'
-//   },
-//   {
-//       name: 'Peach Cobbler',
-//       image: 'https://i.postimg.cc/cH1mDxnD/peach-cobbler.jpg',
-//       description: 'Peach cobbler is out of this world! Uber sweet, as if you weren\'t eating a fruit from a tree. Eat this mango alone or after you eat any others, it takes sweetness to a whole new level.'
-//   }
-// ]
+// import 'Mangoes' from './components/Mangoes.js';
 
 // let counter = 0;
 
@@ -37,6 +10,11 @@ function App() {
       <img id="mango-photo" src="" alt="" />
       {/* <h2 id="mango-name"></h2> */}
       <p id="mango-description"></p>
+
+      {/* {mangoArray.map = (mango =>
+        <p key={mango.name}>{mango.name}{mango.description}</p>
+      )} */}
+
       <div className="button-container">
         <button id="next" className="btn">Next</button>
         <button id="previous" className="btn">Previous</button>
